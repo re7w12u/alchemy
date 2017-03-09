ef9bb9e0dd80c3d6b62e4dc80f2c1b7a87c81f1b
SESA260501 <SESA260501@RDITS00657P>
2017-03-09 22:29:53 +0100

    small adjustments...

--- AlchemyPoc/Program.cs
+++ AlchemyPoc/Program.cs
@@ -22,37 +22,7 @@ namespace AlchemyPoc
             Console.ReadLine();
         }
 
-        private static void Method1(string url)
-        {
-            using (WebClient client = new WebClient())
-            {
-                string data = File.ReadAllText("./../../recette.txt", Encoding.UTF8);
-
-                client.Encoding = Encoding.UTF8;
-                client.Headers["apikey"] = "3ea96d21f40035354488d86b20387df5fdb6a6d1";
-                client.Headers["outputMode"] = "json";
-                client.Headers["extract"] = "entities,keywords";
-                client.Headers["model"] = "c5737a8e-14c0-44e4-ab78-11886b0129dd";
-                client.Headers["sentiment"] = "1";
-                client.Headers["maxRetrieve"] = "1";
-                client.Headers["url"] = "https://www.ibm.com/us-en/";
-                client.Headers["text"] = data;
-
-                var reqparm = new System.Collections.Specialized.NameValueCollection();
-                client.UploadStringCompleted += (object sender, UploadStringCompletedEventArgs e) =>
-                {
-                    Console.WriteLine("response received...");
-                    if (!String.IsNullOrWhiteSpace(e.Error.Message)) WriteResult(e.Error.Message);
-                    else WriteResult(e.Result);
-
-                    Environment.Exit(0);
-                };
-
-                client.UploadStringAsync(new Uri(url), data);
-                Console.WriteLine("request sent...");
-            }
-        }
-
+     
         private static void WriteResult(string message)
         {
             File.WriteAllText(String.Format("output_{0}.txt", DateTime.Now.Ticks), message);
@@ -64,10 +34,10 @@ namespace AlchemyPoc
             string data = File.ReadAllText("./../../recette.txt", Encoding.UTF8);
             List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
 
-            postData.Add(new KeyValuePair<string, string>("apikey", "3ea96d21f40035354488d86b20387df5fdb6a6d1"));
+            postData.Add(new KeyValuePair<string, string>("apikey", "xxx"));
             postData.Add(new KeyValuePair<string, string>("outputMode", "json"));
             postData.Add(new KeyValuePair<string, string>("extract", "entities,keywords"));
-            postData.Add(new KeyValuePair<string, string>("model", "c5737a8e-14c0-44e4-ab78-11886b0129dd"));
+            postData.Add(new KeyValuePair<string, string>("model", "rxxx"));
             postData.Add(new KeyValuePair<string, string>("sentiment", "1"));
             postData.Add(new KeyValuePair<string, string>("url", "https://www.ibm.com/us-en/"));
             postData.Add(new KeyValuePair<string, string>("text", data));
@@ -95,7 +65,6 @@ namespace AlchemyPoc
                     {
                         return null;
                     }
-                    //return await response.Content.ReadAsAsync<TResult>();
                 }
             }
         }
